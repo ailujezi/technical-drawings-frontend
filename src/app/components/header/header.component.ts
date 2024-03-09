ff7e855cd72e841e6c2a94d407edd4f656de6ca7
Julia Ritz <ritz.julia1@gmail.com>
2024-03-09 17:29:46 +0100

    Add Dashboard and logout; Fix dialog windows

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,18 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 
 import {MatIconModule} from '@angular/material/icon'; 
+import { MatDialog } from '@angular/material/dialog';
 import {MatGridListModule} from '@angular/material/grid-list'; 
 import {MatToolbarModule} from '@angular/material/toolbar'; 
+import { ProjectDashboardComponent } from '../project-dashboard/project-dashboard.component';
 
+4
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatGridListModule, MatToolbarModule, MatIconModule],
+  imports: [MatGridListModule, MatToolbarModule, MatIconModule, ProjectDashboardComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -17,8 +21,22 @@ export class HeaderComponent {
 
   isMenuOpenValue: boolean = true;
 
+  constructor(public dialog: MatDialog, private router: Router) {}
+
   toggleMenu() {
     this.isMenuOpenValue = !this.isMenuOpenValue;
     this.isMenuOpen.emit(this.isMenuOpenValue);
+  }
+
+  openDashboard() {
+    const dialogRef = this.dialog.open(ProjectDashboardComponent, {
+      width: '600px',
+      data: {
+      }
+    }); 
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
   }
 }
